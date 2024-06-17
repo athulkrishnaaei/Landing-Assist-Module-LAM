fc54edbfe1b78de2cd64464343124dae5abf4c80
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-06-17 13:49:08 +0200

    added porsac_v2_function

--- ros2/build/cpp/cmake_install.cmake
+++ ros2/build/cpp/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/athul/uav/Landing-Assist-Module-LAM/ros2/src/cpp
+# Install script for directory: /home/athul/Landing-Assist-Module-LAM/ros2/src/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/athul/uav/Landing-Assist-Module-LAM/ros2/install/cpp")
+  set(CMAKE_INSTALL_PREFIX "/home/athul/Landing-Assist-Module-LAM/ros2/install/cpp")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -37,11 +37,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -50,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/athul/uav/Landing-Assist-Module-LAM/ros2/build/cpp/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/athul/Landing-Assist-Module-LAM/ros2/build/cpp/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
