88dcae91c9716f190531b350d3def3e3d97f19e8
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-06-17 13:49:08 +0200

    added porsac_v2_function

--- ros2/build/plane_detection/CMakeFiles/plane_detection_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for plane_detection_uninstall.
-# This may be replaced when dependencies are built.
--- ros2/build/pointcloud_publisher/prefix_override/sitecustomize.py
+++ ros2/build/pointcloud_publisher/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/athul/uav/Landing-Assist-Module-LAM/ros2/install/pointcloud_publisher'
+sys.prefix = sys.exec_prefix = '/home/athul/Landing-Assist-Module-LAM/ros2/install/pointcloud_publisher'
