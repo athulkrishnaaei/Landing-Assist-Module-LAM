de90a6241e48e63e9056bbe1e46406e54f367101
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-07-11 14:55:51 +0200

    cleaned workspace and removed gitignore

--- ros2/build/pointcloud_plane_segmentation/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/athul/Landing-Assist-Module-LAM/ros2/install/pointcloud_plane_segmentation'
