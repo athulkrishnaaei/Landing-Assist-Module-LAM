e5ea5ab009574a348b49a832e4c85265e2cb2a80
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-07-23 12:52:48 +0000

    tested octomap package and saved the map as .bt file

---
+++ build/octomap_ros/CMakeFiles/test_octomap_ros.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_octomap_ros.dir/test/test_conversions.cpp.o"
+  "test_octomap_ros"
+  "test_octomap_ros.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_octomap_ros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
