5000e91b3abf4c15e6a8b2984021aac1572896d9
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-07-23 12:52:48 +0000

    tested octomap package and saved the map as .bt file

---
+++ build/octomap_ros/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /opt/ros/foxy/src/gtest_vendor
+# Build directory: /home/airsim_user/Downloads/Landing-Assist-Module-LAM/build/octomap_ros/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
