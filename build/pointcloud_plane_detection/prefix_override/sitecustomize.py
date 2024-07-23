f5a291626a04ad3dbfd3768a2615126a17c5bebf
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-07-23 12:52:48 +0000

    tested octomap package and saved the map as .bt file

---
+++ build/pointcloud_plane_detection/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/airsim_user/Downloads/Landing-Assist-Module-LAM/install/pointcloud_plane_detection'
