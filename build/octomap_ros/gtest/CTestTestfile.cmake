fecb18ea7309523c33ba5bfb17dbbe73413554a3
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-07-24 11:22:02 +0200

    added documentation to work with octomap

--- build/octomap_ros/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/foxy/src/gtest_vendor
-# Build directory: /home/airsim_user/Downloads/Landing-Assist-Module-LAM/build/octomap_ros/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/pointcloud_publisher/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/airsim_user/Downloads/Landing-Assist-Module-LAM/install/pointcloud_publisher'
