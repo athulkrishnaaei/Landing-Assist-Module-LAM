1722c9cdbb4ff45a87081a92d385053fcbdacda4
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-07-31 12:13:12 +0200

    removed unwanted files and tested the package with documentation

--- build/octomap_ros/CMakeFiles/octomap_ros_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/octomap_ros_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/octomap_ros_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
